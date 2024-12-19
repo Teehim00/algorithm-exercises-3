a5c860cfd680a91793da876c06bb80fa3d7f78e1
Amornthep <amornthep.tee@gmail.com>
2024-12-19 16:40:04 +0700

    Test:ALGORITHMS

--- ex-1.js
+++ ex-1.js
@@ -1,11 +1,27 @@
 function findStudentIndex(students, searchStudent) {
   // Start coding here
+  let left = 0;
+  let right = students.length - 1;
+  while (left <= right) {
+    let name = Math.floor((left + right) / 2);
+    if (students[name] === searchStudent) {
+      return name;
+    } else if (students[name] < searchStudent) {
+      left = name + 1;
+    } else {
+      right = name - 1;
+    }
+  }
+  return -1;
 }
 
 /* 
   1. ควรใช้ Search Algorithm ใดในการแก้โจทย์นี้ 
+  ----- ตอบ binarySearch
   2. ให้เหตุผลประกอบว่าทำไมถึงเลือกใช้ Search Algorithm นี้
+  ----- ตอบ ข้อมูลไม่ได้เรียงลำดับตัวอักษร เลยเลือกใช้ binarySearch
   3. ให้ระบุว่า Big O Notation ของ Function นี้คือเท่าไร เพราะอะไร
+  ----- ตอบ O(log n) **** อันนี้เปิดดูครับ ลืมครับผม
 */
 
 const students = [
@@ -19,5 +35,6 @@ const students = [
   "John",
 ];
 
+findStudentIndex(students, "John");
 console.log(findStudentIndex(students, "John")); // 7
 console.log(findStudentIndex(students, "Anna")); // -1
--- ex-3.js
+++ ex-3.js
@@ -1,11 +1,24 @@
 function sortByRating(shops) {
   // Start coding here
+  for (let i = 0; i < shops.length - 1; i++) {
+    for (let j = 0; j < shops.length - 1; j++) {
+      if (shops[j].rating < shops[j + 1].rating) {
+        let titles = shops[j];
+        shops[j] = shops[j + 1];
+        shops[j + 1] = titles;
+      }
+    }
+  }
+  return shops;
 }
 
 /* 
-  1. ควรใช้ Algorithm ใดในการแก้โจทย์นี้ 
+  1. ควรใช้ Algorithm ใดในการแก้โจทย์นี้
+  ----- ตอบ Bubble Sort
   2. ให้เหตุผลประกอบว่าทำไมถึงเลือกใช้ Algorithm นี้
+  ----- ตอบ Bubble Sort เพราะต้องเข้าถึงreating แต่ละอันที่อยู่ใน arrar object และต้องจัดเรียง reating ให้เรียงจากมากไปน้อย
   3. ให้ระบุว่า Big O Notation ของ Function นี้คือเท่าไร เพราะอะไร
+  ----- ตอบ O(n²) เพราะใช้ loop ซ่อน loop ***** อันนี้ผมเปิดดูครับ ลืมครับ
 */
 
 const shops = [
